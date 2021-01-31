03963c160ea62d950b14316a9f518c5e622f1c34
Fatma Orsun <fatmaorsun2@gmail.com>
2021-01-31 13:29:43 -0500

    "Security and Authentication Config"

--- src/main/java/com/bank/config/SecurityConfiguration.java
+++ src/main/java/com/bank/config/SecurityConfiguration.java
@@ -5,16 +5,21 @@ package com.bank.config;
  * */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import com.bank.service.impl.UserDetailsServiceImpl;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
+@Configuration
+
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -42,7 +47,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		return NoOpPasswordEncoder.getInstance();
 	}
 	
-	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+	    return super.authenticationManagerBean();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
